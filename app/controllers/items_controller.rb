bbfe78e36bea35e3b2408ab7685c42fa8401d35f
pejman mowloudi <pejmanmowloudi@gmail.com>
2019-08-28 14:30:09 +0430

    Index and show Actions and View Files

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,29 @@
 class ItemsController < ApplicationController
 	
+	before_action :find_item ,only: [:show, :edit, :destroy, :update]
 	def index
+		@items = Item.all.order("created_at DESC")
+	end
+
+	def new
+		@item = Item.new
+	end
+	def show
+	end
+
+	def create
+		@item = Item.new(item_params)
+		if @item.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+	private
+	def item_params
+		params.require(:item).permit(:title , :description)
+	end
+	def find_item
+		@item =Item.find(params[:id])
 	end
 end
