c76d65ec4656f79dd4316e472df50700f522f848
pejman mowloudi <pejmanmowloudi@gmail.com>
2019-08-28 15:12:57 +0430

    Show and Edit on Pages

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,11 +19,30 @@ class ItemsController < ApplicationController
 			render 'new'
 		end
 	end
+
+	def edit
+	end
+	def destroy
+		@item.destroy
+		redirect_to root_path
+	end
+
+    def update
+			if @item.update(item_params)
+			    redirect_to  item_path(@item)
+			else
+				render 'edit'
+			end
+	end
+
 	private
+
 	def item_params
 		params.require(:item).permit(:title , :description)
 	end
+
 	def find_item
 		@item =Item.find(params[:id])
 	end
+
 end
